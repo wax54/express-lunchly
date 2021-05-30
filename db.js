e1fd17eea81361fc2a5645b5c371db2a89d331b5
sam <wax654@gmail.com>
2021-05-30 14:58:23 -0700

    Updated DB to for tests and my dev setup

--- db.js
+++ db.js
@@ -2,7 +2,22 @@
 
 const pg = require("pg");
 
-const db = new pg.Client("postgresql:///lunchly");
+const { user, password, host, port } = require('./dbSecrets.json');
+
+let DB_URI = `postgresql://`;
+if (user) {
+    DB_URI += `${user}:${password}@${host}:${port}/`;
+} else {
+    DB_URI += `/`;
+}
+
+if (process.env.NODE_ENV === 'test') {
+    DB_URI += 'lunchly_test';
+} else {
+    DB_URI += 'lunchly';
+}
+
+let db = new pg.Client({ connectionString: DB_URI });
 
 db.connect();
 
