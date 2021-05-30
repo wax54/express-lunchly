d1a37198196b82cfa1f23d7fdf43a5632d7e6c1b
sam <wax654@gmail.com>
2021-05-30 14:58:35 -0700

    added customer.fullName() functionality

--- models/customer.js
+++ models/customer.js
@@ -59,6 +59,11 @@ class Customer {
     return await Reservation.getReservationsForCustomer(this.id);
   }
 
+  /** get the full name of the customer */
+  fullName(){
+    return `${this.firstName} ${this.lastName}`; 
+  }
+
   /** save this customer. */
 
   async save() {
